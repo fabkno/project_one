4516adcf67faa368bef21413c410b458223da090
fabkno <fabian_knoch@gmx.de>
2018-01-07 17:38:14 +0100

    changed updater

--- update/update_stock.py
+++ update/update_stock.py
@@ -23,22 +23,37 @@ current_path = os.getcwd()
 for _country in ListOfCountries:
 
 	os.chdir(current_path)
-	
+
 	path = '../data/raw/'+_country+'/'
 	
 	ListOfIndices = next(os.walk(path))[1]
 
 	for _index in ListOfIndices:
-
+		os.chdir(current_path)
 		os.chdir(path+_index)
+	
 
 		if os.path.isfile('ListOfCompanies.csv'):
 			labels = pd.read_csv('ListOfCompanies.csv')['Label']
 
-			for _label in labels:
-				stock_prize = pdr.get_data_yahoo(_label,start,end)
-				stock_prize.to_csv(_label+'.csv')
-				print "Stock ",_label, " updated"
+			if len(labels) >=1:
+
+				for _label in labels:
+					
+					try:
+						stock_prize = pdr.get_data_yahoo(_label,start,end)
+						stock_prize.to_csv(_label+'.csv')
+						print "Stock ",_label, " updated"
+
+					except RemoteDataError:
+						print "No information for ticker ", _label
+						continue
+
+			else:
+				print "Searched index does not have any entries"
 		else:
-			print country, ": Index : ",index, " not found"
+				print country, ": Index : ",index, " not found"
+
+		print "########## Index ", _index, " successfully updated #########\n\n"
+
 
